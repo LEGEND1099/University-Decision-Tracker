167820d655384c135c63cf1ae337e953bafb0635
Deepak Kumar Jha <146748807+LEGEND1099@users.noreply.github.com>
2025-02-02 12:53:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,19 +15,19 @@ import os
 
 
 # Embed the service account credentials directly for testing purposes (remove in production)
-service_account_json = """
+service_account_json = f"""
 {
-  "type": os.getenv(type),
-  "project_id": os.getenv(project_id),
-  "private_key_id": os.getenv(private_key_id),
-  "private_key": os.getenv(private_key),
-  "client_email": os.getenv(client_email),
-  "client_id": os.getenv(client_id),
-  "auth_uri": os.getenv(auth_uri),
-  "token_uri": os.getenv(token_uri),
-  "auth_provider_x509_cert_url": os.getenv(auth_provider_x509_cert_url),
-  "client_x509_cert_url": os.getenv(client_x509_cert_url),
-  "universe_domain": os.getenv(universe_domain),
+  "type": {os.getenv(type)},
+  "project_id": {os.getenv(project_id)},
+  "private_key_id": {os.getenv(private_key_id)},
+  "private_key": {os.getenv(private_key)},
+  "client_email": {os.getenv(client_email)},
+  "client_id": {os.getenv(client_id)},
+  "auth_uri": {os.getenv(auth_uri)},
+  "token_uri": {os.getenv(token_uri)},
+  "auth_provider_x509_cert_url": {os.getenv(auth_provider_x509_cert_url)},
+  "client_x509_cert_url": {os.getenv(client_x509_cert_url)},
+  "universe_domain": {os.getenv(universe_domain)}
 }
 """
 
